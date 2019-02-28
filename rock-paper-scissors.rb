6c1c8ac6b05e10aaaaa1cf16b0e2890bb2644b57
yummta <yummta@gmail.com>
2019-02-27 20:09:11 -0500

    corrections rock-paper-scissors

--- rock-paper-scissors.rb
+++ rock-paper-scissors.rb
@@ -3,23 +3,21 @@
 # rps('paper','paper') // Draw!
 
 def rps (player1, player2)
+    return "Draw!" if player1 === player2
     win_player_1 = "Player 1 won!"
     win_player_2 = "Player 2 won!"
     draw = "Draw!"
 
     if player1 == "paper"
-        (player2 == "scissors") ? return win_player_1 : return win_player_2
-        return win_player_1 if player2 == "scissors"
-        return win_player_2 if player2 == "rock"
-        return draw if player2 == "paper"
+        return win_player_1 if player2 == "rock"
+        return win_player_2 if player2 == "scissors"
     elsif player1 == "scissors"
-        return win_player_1 if player2 == "paper"
-        return win_player_2 if player2 == "rock"
-        return draw if player2 == "scissors"
+        return win_player_1 if player2 == "rock"
+        return win_player_2 if player2 == "paper"
     else
-        return win_player_1 if player2 == "scissors" return win_player_2 if player2 == "paper"
-        return draw if player2 == "rock"
+        return win_player_1 if player2 == "scissors"
+        return win_player_2 if player2 == "paper"
     end
 end
 
-puts rps('paper','paper')
+puts rps('paper','rock')
